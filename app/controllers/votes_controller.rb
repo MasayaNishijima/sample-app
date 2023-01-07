2a6e8a9bf354a37ceca491d840afa1a25e60adc0
github sushi723 <26241549+sushi723@users.noreply.github.com>
2023-01-07 10:46:39 +0900

    fix: #3 投票できない問題の修正

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -65,6 +65,6 @@ class VotesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def vote_params
-      params.require(:vote).permit(:name)
+      params.require(:vote).permit(:title)
     end
 end
