9b1aa9dcbd6bcc6f32a5dab452507dee80c571fa
github sushi723 <26241549+sushi723@users.noreply.github.com>
2023-01-21 10:32:00 +0900

    refactor: #3 不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :votes
-  resources :choices do
-    patch "count_up", on: :member
-  end
+  patch 'choices/:id', to: 'choices#count_up', as: 'count_up_choice'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
